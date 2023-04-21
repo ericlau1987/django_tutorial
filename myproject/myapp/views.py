06e191f5fb1d5e7c4fc193b7e957d19bd559b5de
Eric Yifu Liu <46115029+ericlau1987@users.noreply.github.com>
2023-04-21 20:40:25 +1000

    updates

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -8,16 +8,20 @@ import urllib
 def index(request):
 
     features = Feature.objects.all
-
-    res = urllib.request.urlopen('https://www.land.vic.gov.au/property-report/property-dashboard2/street_suggestions.json?extraQuery=amendment-id&profile=amendment-id&partial_query=29%20LONGSTAFF%20ROAD%20BAYSWATER%203153').read()
+    no_street = 23
+    street_name = 'Longstaff' 
+    street_type = 'Road'
+    suburb = 'Bayswater'
+    postcode = 3153
+    res = urllib.request.urlopen(f'https://www.land.vic.gov.au/property-report/property-dashboard2/street_suggestions.json?extraQuery=amendment-id&profile=amendment-id&partial_query={no_street}%20{street_name.upper()}%20{street_type.upper()}%20{suburb.upper()}%20{postcode}').read()
     data = json.loads(res.decode('utf-8'))
-    # key = data[0]['key']
-    key = 'GST7YMc0AM9UOsKtGTyVGST7YMc0AM9UOsExAi9XOc50YTc2KQWmObktGMytaikZQBkKM5NKMoFF'
+    key = data[0]['key']
     pfi_data = urllib.request.urlopen(f'https://www.land.vic.gov.au/property-report/property-dashboard2/get_street_key.json?query={key}').read()
     pfi = json.loads(pfi_data.decode('utf-8'))['pfi']
     # address_data = urllib.request.urlopen(f'https://www.land.vic.gov.au/property-report/property-dashboard2/get_street_address.json?query={pfi}').read()
     # address = json.loads(address_data.decode('utf-8'))['pfi']
-    return render(request, 'index.html', {'features': features, 'result': pfi}) # link it to the index.html in the template
+    url = f'https://production-detailed-report-pdf.s3-ap-southeast-2.amazonaws.com/{no_street}-{street_name}-{street_type}-{suburb}-(ID{pfi})-Detailed-Property-Report.pdf'
+    return render(request, 'index.html', {'features': features, 'key': key, 'result': pfi, 'url': url}) # link it to the index.html in the template
 
 def counter(request):
     text = request.POST['text']
